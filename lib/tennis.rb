3e1df23a196245c0e334de9fea69a771e0b895f1
jfher <Fercho_Sk_669@hotmail.com>
2015-11-16 19:09:10 -0400

    fixed

--- lib/tennis.rb
+++ lib/tennis.rb
@@ -7,22 +7,25 @@ class Tennis
   end
 
   def score
-    if(@jugador_1.score == "40" && @jugador_2.score == "40")
-      'deuce'
-    elsif (@jugador_1.puntos == 4 && @jugador_2.score == "40")
-      'advantaje jugador_1'
-    elsif (@jugador_2.score == 4 && @jugador_1.score == "40")
-      'advantaje jugador_2'
-    elsif ((@jugador_1.puntos >= 4 && @jugador_2.puntos < 3) || (@jugador_1.score == "adv"  && @jugador_2.score == "40"))
-      'winner jugador_1'
-    elsif ((@jugador_2.puntos >= 4 && @jugador_1.puntos < 3) || (@jugador_2.score == "adv"  && @jugador_1.score == "40"))
-      'winner jugador_2'
-    elsif (@jugador_2.puntos == 4 && @jugador_1.puntos == 4)
-      'deuce'
-    else
-      "#{@jugador_1.score} - #{@jugador_2.score}"
-    end
+      if(@jugador_1.score == "40" && @jugador_2.score == "40")
+        'deuce'
+      elsif (@jugador_1.puntos == 4 && @jugador_2.score == "40")
+        'advantaje jugador_1'
+      elsif (@jugador_2.puntos == 4 && @jugador_1.score == "40")
+        'advantaje jugador_2'
+      elsif ((@jugador_1.puntos >= 4 && @jugador_2.puntos < 3) || (@jugador_1.score == "adv"  && @jugador_2.score == "40"))
+        'winner jugador_1'
+      elsif ((@jugador_2.puntos >= 4 && @jugador_1.puntos < 3) || (@jugador_2.score == "adv"  && @jugador_1.score == "40"))
+        'winner jugador_2'
+      elsif (@jugador_2.puntos == 4 && @jugador_1.puntos == 4)
+        @jugador_2.empate
+        @jugador_1.empate
+        'deuce'
+      else
+        "#{@jugador_1.score} - #{@jugador_2.score}"
+      end
   end
+
 
   def score_jugador(jugador)
     if jugador == 1
